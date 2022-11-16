12878a3a1182c6d875515a7f45749793b7057087
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2022-11-16 13:53:47 -0300

    Project created

---
+++ backend/src/test/java/com/wlamonserrat/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlamonserrat.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
